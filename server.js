0fd26a38a98c899a77469ca42766e3dc498f5e31
GioLucci <141793843+Vannasaur@users.noreply.github.com>
2023-09-20 20:18:02 -0700

    updated server.js file, safe revert point

--- server.js
+++ server.js
@@ -8,17 +8,25 @@ const PORT = 3001;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
 // API Routes
 // GET /api/notes should read the db.json file and return all saved notes as JSON
-app.get('/api/notes', (req,res) => {
-    res.status(200).json(`${req.method} request received to get notes`);
-
-    console.info(`${req.method} request received to get notes`);
-});
+app.get('/api/notes', async (req, res) => {
+    try {
+        const savedNote = await fs.readfile('./db/db.json', 'utf8');
+        const savedNoteParse = JSON.parse(savedNote);
+        return res.json(savedNoteParse);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+})
 // POST /api/notes should receive a new note to save on the request bocy, add it to the db.json file and then return the new note to the client
 // give each note a unique id when it's saved from uuid npm package
 app.post('/api/notes', async (req, res) => {
@@ -50,12 +58,12 @@ app.post('/api/notes', async (req, res) => {
                 `Note for ${newNote.title} has been written to JSON file`
             )
 
-                const response = {
-                    status: 'success',
-                    body: newNote,
-                };
-                
-                return res.status(201).json(response);
+            const response = {
+                status: 'success',
+                body: newNote,
+            };
+
+            return res.status(201).json(response);
         } else {
             return res.status(500).json('Error in posting review');
         }
@@ -68,7 +76,7 @@ app.post('/api/notes', async (req, res) => {
 // HTML Routes
 // GET /notes should return the notes.html file
 app.get('/notes', (req, res) => {
-    return res.sendFile(path.join(__dirname, 'public/notes.html'))
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
 })
 // GET * should return the index.html file
 app.get('*', (req, res) => {
